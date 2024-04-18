ab15c328270549067fdb0574b4d03b6ea5a648c7
Muhammad Ahmer Raza <ahmer183gamerz@gmail.com>
2024-04-19 03:01:41 +0500

    Timeline Stepper Component

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import VerticalStepper from './components/VerticalStepper'
+
+export default function App() {
+  return (
+    <div className='app'>
+    <VerticalStepper />
+    </div>
+
+  )
+}
